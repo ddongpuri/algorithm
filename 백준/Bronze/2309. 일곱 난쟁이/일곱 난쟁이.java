d34a0f8ac19cc1ae04ee46062ac533f21e67baac
ddongpuri <96704446+ddongpuri@users.noreply.github.com>
2022-09-13 20:37:08 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,37 +1,32 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    int [] heights = new int[10]; // 9 난쟁이의 키를 담을 int형 배열 준비 
-    int sum = 0; // 9 난쟁이의 키의 합 
-    int tmp = 0;
-    int out1 = 0; // 7난쟁이에서 제외될 난쟁이의 인덱스값을 저장할 변수 1
-    int out2 = 0; // 7난쟁이에서 제외될 난쟁이의 인덱스값을 저장할 변수 2
 
-    for (int i = 1; i <= 9; i++) { // 9 난쟁이의 키를 배열에 담는다. 
-      heights[i] = sc.nextInt();
+  public static void main (String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int[] heights = new int[10];
+    int sum = 0;
+    int out1 = 0;
+    int out2 = 0;
+
+    for (int i = 1 ; i <= 9; i++) {
+      heights[i] = Integer.parseInt(br.readLine());
+    }
+
+    for (int i = 1; i <= 9; i++) {
       sum += heights[i];
     }
 
+    Arrays.sort(heights);
 
-    for (int i = 1; i <= 8; i++) { // 난쟁이 키 오름차순으로 정렬하기 
-      for (int j = i + 1; j <=9; j++) {
-        if (heights[i] >= heights[j]) {
-          tmp = heights[j];
-          heights[j] = heights[i];
-          heights[i] = tmp;
-        }
-      }
-    }
-
-
-    for (int j = 1; j <= 8; j++) { // 9난쟁이 키의 총합에서 두 개를 뺏을 때 100 되는 배열 요소 구하기 
-      for (int k = j + 1; k <= 9; k++) {
-        if ((sum - heights[j] - heights[k]) == 100) {
-
-          out1 = j;
-          out2 = k;
+    for (int i = 1; i < 9; i++) {
+      for (int j = i + 1; j <= 9; j++) {
+        if ((sum - heights[i] - heights[j]) == 100) {
+          out1 = i;
+          out2 = j; 
         }
       }
     }
@@ -39,12 +34,11 @@ public class Main {
     heights[out1] = 0;
     heights[out2] = 0;
 
-    for (int i = 1; i <=9;i++) {
-      if (heights[i] != 0) {
-        System.out.println(heights[i]);
+    for (int height : heights) {
+      if (height != 0) {
+        System.out.println(height);
       }
     }
-
-    sc.close();
+    br.close();
   }
 }
