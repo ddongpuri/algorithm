2cc8b8db819de48468da36268b98663ff714fc63
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-11-27 19:01:14 +0900

    [Bronze V] Title: A+B, Time: 220 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,12 +1,13 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a, b;
-        a = sc.nextInt();
-        b = sc.nextInt();
-        
-        System.out.println(a + b);
-    }
+public class Main {
+  public static void main(String[] args) throws Exception {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + b);
+
+  }
 }
