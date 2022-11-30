b7f49afa29564b1487df22f8564a7e7857e1a230
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-11-30 11:14:47 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,17 +1,22 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Main {
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
     String str = br.readLine();
 
     for (int i = 'a'; i <= 'z'; i++) {
-      System.out.printf("%d ", str.indexOf((char)i));
+      bw.append(String.valueOf(str.indexOf((char)i)));
+      bw.append(' ');
     }
 
+    bw.flush();
   }
 }
