cb7473cac9c403102ba39c1a8edd632463591b5c
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-12-08 00:56:54 +0900

    [Bronze V] Title: 사파리월드, Time: 204 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws NumberFormatException, IOException {
+
+    Scanner sc = new Scanner(System.in);
+      
+    long a = sc.nextLong();
+    long b = sc.nextLong();  
+    
+    System.out.println(Math.abs(a - b));
+
+    sc.close();
+  }
+}
