a761509b0ef1f2b972bd6499d9c2197ab5437bca
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-12-14 21:46:46 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1112 ms, Memory: 172560 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,27 +5,24 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
+  static int n; // 상근이가 가지고 있는 숫자 카드 개수 
+  static int m; // 상근이가 몇 개 가지고 있는 숫자 카드인지 구해야 할 정수의 개수 
+  static HashMap<Integer, Integer> map; 
   static StringTokenizer stk;
-  static int n;
-  static int m;
-  static HashMap<Integer, Integer> map;
 
   public static void main(String[] args) throws NumberFormatException, IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     n = Integer.parseInt(br.readLine());
-    map = new HashMap<>();
+    map = new HashMap<>(); // 숫자 - 개수 쌍을 저장할 HashMap
     StringBuffer sb = new StringBuffer();
 
     stk = new StringTokenizer(br.readLine());
     for (int i = 0; i < n; i++) {
       int num = Integer.parseInt(stk.nextToken());
 
-      if(!map.containsKey(num)) {
-        map.put(num, 1);
-      } else {
-        map.put(num, map.get(num) + 1);
-      }
+      map.put(num, map.getOrDefault(num, 0) + 1);  
+        
     }
 
     m = Integer.parseInt(br.readLine());
@@ -33,12 +30,13 @@ public class Main {
     for (int i = 0; i < m; i++) {
       int num = Integer.parseInt(stk.nextToken());
 
-      if (map.containsKey(num)) {
-        sb.append(map.get(num) + " ");
-
-      } else {
-        sb.append(0 + " ");
-      }
+      //      if (map.containsKey(num)) {
+      //        sb.append(map.get(num) + " ");
+      //
+      //      } else {
+      //        sb.append(0 + " ");
+      //      }
+      sb.append(map.getOrDefault(num, 0)).append(" ");
     }
 
     System.out.println(sb.toString());
