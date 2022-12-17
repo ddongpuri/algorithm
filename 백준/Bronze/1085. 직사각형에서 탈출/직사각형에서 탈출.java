f287b3a201eedaa902f4d94e632d98b889bc08ac
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-12-17 23:53:05 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 208 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    Scanner sc = new Scanner(System.in);
+
+    int x = sc.nextInt(); 
+    int y = sc.nextInt(); 
+    int w = sc.nextInt(); 
+    int h = sc.nextInt();
+
+    System.out.println(Math.min(Math.min(x, w - x), Math.min(y, h - y)));
+
+  }
+}
