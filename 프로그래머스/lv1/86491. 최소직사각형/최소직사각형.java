873e035d4ae384aac5cf683df6147ab4152aa146
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2022-12-28 21:14:53 +0900

    [level 1] Title: 최소직사각형, Time: 1.16 ms, Memory: 86.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int tmp;
+        int w = 0;
+        int h = 0;
+        
+        for (int i = 0; i < sizes.length; i++) {
+            if (sizes[i][0] > sizes[i][1]) {
+                tmp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = tmp;
+            }
+        }
+        
+        for (int i = 0; i < sizes.length; i++) {
+            if (w < sizes[i][0]) w = sizes[i][0];
+            if (h < sizes[i][1]) h = sizes[i][1];
+        }
+        
+        answer = w * h;
+        
+        return answer;
+    }
+}
