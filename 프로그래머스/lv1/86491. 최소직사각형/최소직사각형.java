2b859fb20c1c4a7b42a9f4a73dfc41e763c6e33e
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-01-04 18:22:27 +0900

    [level 1] Title: 최소직사각형, Time: 1.64 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.java
@@ -1,10 +1,12 @@
 class Solution {
     public int solution(int[][] sizes) {
         int answer = 0;
-        int tmp;
-        int w = 0;
-        int h = 0;
         
+        int tmp; // 값의 자리를 바꿀 때 사용할 임시변수
+        int w = 0; // 지갑의 가로길이
+        int h = 0; // 지갑의 세로길이
+        
+        // 1. 명함의 가로세로 길이 중 더 큰 것이 뒤로 오게끔 만든다. 
         for (int i = 0; i < sizes.length; i++) {
             if (sizes[i][0] > sizes[i][1]) {
                 tmp = sizes[i][0];
@@ -13,6 +15,7 @@ class Solution {
             }
         }
         
+        // 2. 앞자리[0], 뒷자리[1] 수들의 최대값을 각 각 구한다.
         for (int i = 0; i < sizes.length; i++) {
             if (w < sizes[i][0]) w = sizes[i][0];
             if (h < sizes[i][1]) h = sizes[i][1];
