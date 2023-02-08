79a34269f47f59bef2d950fe5a4f23b1b4b6ecf3
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-02-08 10:37:41 +0900

    [Silver II] Title: 그르다 김가놈, Time: 604 ms, Memory: 93980 KB -BaekjoonHub

--- 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
+++ 백준/Silver/18113. 그르다 김가놈/그르다 김가놈.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -23,7 +22,7 @@ public class Main {
     m = Integer.parseInt(stk.nextToken());
     int sum = 0;
 
-    list = new LinkedList<>();
+    list = new ArrayList<>();
 
     for (int i = 0; i < n; i++) {
       int a = Integer.parseInt(br.readLine());
