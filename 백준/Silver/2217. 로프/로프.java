47cd4456674b4e988cd012c394c6e070c5c24d19
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-04-03 23:00:02 +0900

    [Silver IV] Title: 로프, Time: 304 ms, Memory: 25248 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,17 +1,18 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) throws Exception {
 
-    Scanner sc = new Scanner(System.in);
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int n = sc.nextInt();
+    int n = Integer.parseInt(br.readLine());
     int[] arr = new int[n];
     int max = 0;
     int cnt = arr.length;
 
-    for (int i = 0; i < arr.length; i++) arr[i] = sc.nextInt();
+    for (int i = 0; i < arr.length; i++) arr[i] = Integer.parseInt(br.readLine());
 
     Arrays.sort(arr);
 
