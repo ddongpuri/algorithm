851c008f35f26dfe8ec5bb720366a803e9e8c9f4
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-04-06 13:25:12 +0900

    [Bronze III] Title: 최소, 최대, Time: 500 ms, Memory: 106420 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,27 +1,27 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
-  public static void main(String[] args) throws NumberFormatException, IOException {
+  public static void main(String[] args) throws Exception {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer stk;
+    StringBuilder sb = new StringBuilder();
 
-    int N = Integer.parseInt(br.readLine());
-    int[] arr = new int[N];
+    int n = Integer.parseInt(br.readLine());
 
-    stk = new StringTokenizer(br.readLine());
+    String[] arr = br.readLine().split(" ");
+
+    int max = Integer.MIN_VALUE;
+    int min = Integer.MAX_VALUE;
 
     for (int i = 0; i < arr.length; i++) {
-      arr[i] = Integer.parseInt(stk.nextToken());
+      int num = Integer.parseInt(arr[i]);
+
+      max = Math.max(max, num);
+      min = Math.min(min, num);
     }
 
-    Arrays.sort(arr);
-
-    System.out.print(arr[0] + " " + arr[arr.length - 1]);
+    System.out.println(min + " " + max);
 
   }
 }
