6b078fda030bb5e67e8cc84d69ee46a89193bbcd
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-04-08 00:14:40 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 208 ms, Memory: 17672 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,24 +1,27 @@
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-
-    int[] fibo = new int[21];
-
-    fibo[0] = 0;
-    fibo[1] = 1;
+  public static void main(String[] args) throws Exception {
 
     Scanner sc = new Scanner(System.in);
+
     int n = sc.nextInt();
 
-    if (n >= 2) {
-      for (int i = 2; i <= n ; i++) {
-        fibo[i] = fibo[i - 1] + fibo[i - 2];
-      }
+    int [] arr = new int[n + 1];
+
+    if (n == 1 || n == 0) {
+      System.out.println(n);
+      return;
     }
 
-    System.out.println(fibo[n]);
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for (int i = 2; i < n + 1; i++) {
+      arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    System.out.println(arr[n]);
 
   }
 }
