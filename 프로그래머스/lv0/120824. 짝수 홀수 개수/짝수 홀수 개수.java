8995d99e5c6b7ad04bde062a41a06817fb9dca06
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-04-22 21:57:51 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.01 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        
+        for (int number : num_list) {
+            if ((number & 1) == 0) answer[0]++;
+            else answer[1]++;
+        }
+        
+        return answer;
+    }
+}
