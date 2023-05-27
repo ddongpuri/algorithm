204c7170a4604a2473a9c88c887a51fcd814b494
Jiyeon choi <96704446+ddongpuri@users.noreply.github.com>
2023-05-27 09:50:52 +0900

    [level 0] Title: 세균 증식, Time: 0.05 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120910. 세균 증식/세균 증식.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int t) {
+        int answer = 1;
+        
+        answer = n * (int)Math.pow(2, t);
+    
+        return answer;
+    }
+}
